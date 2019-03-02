0384abb43e9a07f720ba48403227b5d99817be51
Wouter de Dood <wdedood@outlook.com>
2019-03-02 10:22:31 +0100

    Changed firmware cmd line

--- 00-updatepi.sh
+++ 00-updatepi.sh
@@ -2,7 +2,9 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 #firmware
-sudo apt-get install rpi-update -Y
-sudo rpi-update
+sudo apt-get install rpi-update && echo Y | sudo rpi-update
 #Change tijdzone
 sudo timedatectl set-timezone Europe/Amsterdam
+
+
+#wget -O - https://raw.githubusercontent.com/wdedood/raspberrypi/master/00-updatepi.sh | sudo bash
