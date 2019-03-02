147321651d1e4065baad0cb99d3ee5fc69f768ed
Wouter de Dood <wdedood@outlook.com>
2019-03-02 13:09:17 +0100

    Created mappings

--- 02-mappings.sh
+++ 02-mappings.sh
@@ -1,11 +1,3 @@
-
-
-
-
-#Restart de pi
-sudo reboot
-
-
 #mappings
 sudo mkdir /home/pi/mappings
 sudo mkdir /home/pi/mappings/nas-popcorn
@@ -13,4 +5,3 @@ sudo mkdir /home/pi/mappings/nas-download
 echo -e '//nas.woutert.net/download /home/pi/mappings/nas-download cifs password=,file_mode=0777,dir_mode=0777 0 0\n' | sudo tee --append /etc/fstab
 echo -e '//nas.woutert.net/popcorn /home/pi/mappings/nas-popcorn cifs password=,file_mode=0777,dir_mode=0777 0 0\n' | sudo tee --append /etc/fstab
 
-#wait for network
